54e87a320b64b461b728f24965d8a565a62a58ee
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2023-02-07 22:10:01 +0100

    add sort users descending

--- AngryPullRequests/AngryPullRequests.Application/Slack/Formatters/DeveloperLoadMessageFormatter.cs
+++ AngryPullRequests/AngryPullRequests.Application/Slack/Formatters/DeveloperLoadMessageFormatter.cs
@@ -14,7 +14,8 @@ namespace AngryPullRequests.Application.Slack.Formatters
             var prsByUser = pullRequestNotificationGroups
                 .SelectMany(prg => prg.Reviewers)
                 .GroupBy(r => r.Login)
-                .Select(group => new { Login = group.Key, Count = group.Count() });
+                .Select(group => new { Login = group.Key, Count = group.Count() })
+                .OrderByDescending(pu => pu.Count);
 
             var userBlocks = new List<Block> { new HeaderBlock { Text = CreatePe($"Opterecenje po developeru :face_with_peeking_eye:") } };
 
