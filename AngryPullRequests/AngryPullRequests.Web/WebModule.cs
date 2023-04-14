c0d56b29e9480699e7fa10232e717f441f5580dc
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2023-04-14 21:28:52 +0200

    use unused service

--- AngryPullRequests/AngryPullRequests.Web/WebModule.cs
+++ AngryPullRequests/AngryPullRequests.Web/WebModule.cs
@@ -24,26 +24,12 @@ namespace AngryPullRequests.Web
 
             builder.RegisterAutoMapper(typeof(AutoMapperProfile).Assembly);
 
-            //builder.RegisterInstance(appConfiguration.Scheduling).SingleInstance();
-            //builder.RegisterInstance(appConfiguration.RepositoryConfiguration).SingleInstance();
-            //builder.RegisterInstance(appConfiguration.SlackConfiguration).SingleInstance();
-            //builder.RegisterInstance(appConfiguration.PullRequestPreferences ?? new PullRequestPreferences()).SingleInstance();
-            //builder.RegisterInstance(appConfiguration.JiraConfiguration ?? new JiraConfiguration()).SingleInstance();
-            //builder.RegisterInstance(appConfiguration.OpenAiConfiguration).SingleInstance();
-
             builder.RegisterType<RunnerHostedService>().As<IHostedService>();
             builder.RegisterType<AngryPullRequestsService>().As<IAngryPullRequestsService>();
             builder.RegisterType<SlackNotifierService>().As<IUserNotifierService>();
             builder.RegisterType<OpenAiCompletionService>().As<ICompletionService>();
             builder.RegisterType<MetricService>().As<IMetricService>();
             builder.RegisterType<PullRequestService>().As<IPullRequestService>();
-
-            //builder.AddSlackNet(
-            //    c =>
-            //        c.UseApiToken(appConfiguration.SlackConfiguration.ApiToken)
-            //            .UseAppLevelToken(appConfiguration.SlackConfiguration.AccessToken)
-            //            .RegisterSlashCommandHandler<AngrySlashCommandHandler>("/apr-all")
-            //);
 
             builder.RegisterType<ForgottenPullRequestsMessageFormatter>().As<ISlackMessageFormatter>();
             builder.RegisterType<DeveloperLoadMessageFormatter>().As<ISlackMessageFormatter>();
