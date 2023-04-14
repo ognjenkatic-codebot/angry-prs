f4e875a6575211dfbf6aa13aa17a6b80fa1cec3a
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2023-04-14 21:27:08 +0200

    remove unnecessary service

--- AngryPullRequests/AngryPullRequests.Web/Services/RunnerHostedService.cs
+++ AngryPullRequests/AngryPullRequests.Web/Services/RunnerHostedService.cs
@@ -1,23 +1,57 @@
 ﻿using AngryPullRequests.Application.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace AngryPullRequests.Web.Services
 {
-    public class RunnerHostedService : RunnerService, IHostedService
+    public class RunnerHostedService : IHostedService
     {
         private readonly CancellationTokenSource _tokenSource = new CancellationTokenSource();
+        private readonly Func<IAngryPullRequestsService> angryPullRequestServiceFactory;
+        private readonly IAngryPullRequestsContext dbContext;
 
         public RunnerHostedService(Func<IAngryPullRequestsService> angryPullRequestServiceFactory, IAngryPullRequestsContext dbContext)
-            : base(angryPullRequestServiceFactory, dbContext) { }
+        {
+            this.angryPullRequestServiceFactory = angryPullRequestServiceFactory;
+            this.dbContext = dbContext;
+        }
 
         public async Task StartAsync(CancellationToken cancellationToken)
         {
-            await Start(_tokenSource.Token);
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Run();
+                await Task.Delay(GetDelayToNextMinute(), cancellationToken);
+            }
         }
 
         public Task StopAsync(CancellationToken cancellationToken)
         {
             _tokenSource.Cancel();
             return Task.CompletedTask;
+        }
+
+        private TimeSpan GetDelayToNextMinute()
+        {
+            var now = DateTimeOffset.UtcNow;
+            var nowPlus = new DateTimeOffset(now.Year, now.Month, now.Day, now.Hour, now.Minute + 1, 5, TimeSpan.Zero);
+            return nowPlus - now;
+        }
+
+        private async Task Run()
+        {
+            var now = DateTimeOffset.UtcNow;
+            var repos = await dbContext.RunSchedules.Include(r => r.Repository).ToListAsync();
+
+            foreach (var repo in repos)
+            {
+                var isNow = repo.TimeOfDay.Hour == now.Hour && repo.TimeOfDay.Minute == now.Minute;
+                isNow = true;
+
+                if (isNow)
+                {
+                    await angryPullRequestServiceFactory().CheckOutPullRequests(repo.Repository.Name, repo.Repository.Owner);
+                }
+            }
         }
     }
 }
