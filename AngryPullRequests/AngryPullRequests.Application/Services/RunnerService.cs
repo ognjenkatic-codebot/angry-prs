bc782ff3607479ff70a8ecf244956c625e6a4574
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2023-04-14 19:40:39 +0200

    simplify delay

--- AngryPullRequests/AngryPullRequests.Application/Services/RunnerService.cs
+++ AngryPullRequests/AngryPullRequests.Application/Services/RunnerService.cs
@@ -31,42 +31,17 @@ namespace AngryPullRequests.Application.Services
         private TimeSpan GetDelayToNextMinute()
         {
             var now = DateTimeOffset.UtcNow;
-            var delay = new DateTimeOffset(now.Year, now.Month, now.Day, now.Hour, now.Minute + 1, 5, TimeSpan.Zero) - now;
-            return delay;
-
-            //var target = new DateTime(
-            //    DateTime.Now.Year,
-            //    DateTime.Now.Month,
-            //    DateTime.Now.Day,
-            //    schedulingConfiguration.Hour,
-            //    schedulingConfiguration.Minute,
-            //    0
-            //);
-            //var today = DateTime.Now;
-
-            //// Check if the target time has already passed for today
-            //if (today.TimeOfDay.TotalMinutes > target.TimeOfDay.TotalMinutes)
-            //{
-            //    // If so, calculate the time until the target time on the next work day
-            //    target = target.AddDays(1);
-            //    while (target.DayOfWeek == DayOfWeek.Saturday || target.DayOfWeek == DayOfWeek.Sunday)
-            //    {
-            //        target = target.AddDays(1);
-            //    }
-            //}
-
-            //// Calculate the timespan until the target time
-            //var timeUntilTarget = target - today;
-            //Console.WriteLine($"Waiting for {timeUntilTarget:c} to next run");
-            //return timeUntilTarget;
+            return new DateTimeOffset(now.Year, now.Month, now.Day, now.Hour, now.Minute + 1, 5, TimeSpan.Zero) - now;
         }
 
         private async Task Run()
         {
+            var now = DateTimeOffset.UtcNow;
             var repos = await dbContext.RunSchedules.Include(r => r.Repository).ToListAsync();
 
             foreach (var repo in repos)
             {
+                var isNow = repo.TimeOfDay.Hour == now.Hour && repo.TimeOfDay.Minute == now.Minute;
                 Console.WriteLine(repo.Repository.Name);
             }
             //await angryPullRequestServiceFactory().CheckOutPullRequests();
