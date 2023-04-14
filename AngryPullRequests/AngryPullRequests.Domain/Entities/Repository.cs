082ffdf2d026a23fe45b9835154f9714d2480cb3
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2023-04-14 19:40:03 +0200

    add owner to repo

--- AngryPullRequests/AngryPullRequests.Domain/Entities/Repository.cs
+++ AngryPullRequests/AngryPullRequests.Domain/Entities/Repository.cs
@@ -7,6 +7,7 @@ namespace AngryPullRequests.Domain.Entities
         public Guid Id { get; set; }
         public Guid AngryUserId { get; set; }
         public string Name { get; set; }
+        public string Owner { get; set; }
         public virtual AngryUser AngryUser { get; set; }
         public virtual RunSchedule RunSchedule { get; set; }
     }
