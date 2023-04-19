55e7f9326b09c6f06fe62afabc9dba191244db6a
Ognjen Katić <44910579+ognjenkatic@users.noreply.github.com>
2023-04-20 00:40:25 +0200

    fix typo

--- AngryPullRequests/AngryPullRequests.Web/Services/RunnerHostedService.cs
+++ AngryPullRequests/AngryPullRequests.Web/Services/RunnerHostedService.cs
@@ -33,8 +33,8 @@ namespace AngryPullRequests.Web.Services
                 // Calculate delay untill next minute, assuming tasks don't run for more than one minute
                 var now = DateTimeOffset.UtcNow;
                 var nowPlus = new DateTimeOffset(now.Year, now.Month, now.Day, now.Hour, now.Minute, 0, TimeSpan.Zero).AddMinutes(1).AddSeconds(5);
-)
-               if (now.Hour == 3 && now.Minute == 0)
+
+                if (now.Hour == 3 && now.Minute == 0)
                 {
                     await mediator.Send(new IndexContributionsCommand(), cancellationToken);
                 }
